fcb7ccc51300e5d585ba84dea3b346cf401278fc
enzocouto <enzocouto@gmail.com>
2017-10-30 17:52:21 -0200

    atualização de dados

--- server/mock-brokers.js
+++ server/mock-brokers.js
@@ -3,84 +3,76 @@
 let brokers = [
     {
         id: 1,
-        firstName: "Enzo",
-        lastName: "Couto",
-        title: "Desenvolvedor Java Senior",
+        name: "Enzo Couto",
+        title: "Corretor Sênior",
         phone: "",
         mobilePhone: "(011)96016-6217",
         email: "enzocouto@gmail.com",
         picture: "https://yt3.ggpht.com/-toLuAGgWHzA/AAAAAAAAAAI/AAAAAAAAAAA/uKQ32uN9g4E/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
     },
-//    {
-//        id: 2,
-//        firstName: "Michael",
-//        lastName: "Jones",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "michael@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg"
-//    },
-//    {
-//        id: 3,
-//        firstName: "Jonathan",
-//        lastName: "Bradley",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "jonathan@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg"
-//    },
-//    {
-//        id: 4,
-//        firstName: "Jennifer",
-//        lastName: "Wu",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "jen@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg"
-//    },
-//    {
-//        id: 5,
-//        firstName: "Olivia",
-//        lastName: "Green",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "olivia@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg"
-//    },
-//    {
-//        id: 6,
-//        firstName: "Miriam",
-//        lastName: "Aupont",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "miriam@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg"
-//    },
-//    {
-//        id: 7,
-//        firstName: "Michelle",
-//        lastName: "Lambert",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "michelle@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg"
-//    },
-//    {
-//        id: 8,
-//        firstName: "Victor",
-//        lastName: "Ochoa",
-//        title: "Senior Broker",
-//        phone: "617-244-3672",
-//        mobilePhone: "617-244-3672",
-//        email: "victor@ionicrealty.com",
-//        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg"
-//    }
+    {
+        id: 2,
+        name: "Mauro Pereira",
+        title: "Corretor Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "michael@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg"
+    },
+    {
+        id: 3,
+        name: "Jonas Brito",
+        title: "Corretor Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "jonathan@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg"
+    },
+    {
+        id: 4,
+        name: "Jennifer Moreira",
+        title: "Corretora Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "jen@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg"
+    },
+    {
+        id: 5,
+        name: "Olivia Martis",
+        title: "Corretora Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "olivia@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg"
+    },
+    {
+        id: 6,
+        name: "Miriam Schimidt",
+        title: "Corretora Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "miriam@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg"
+    },
+    {
+        id: 7,
+        name: "Michelle dos Santos",
+        title: "Corretora Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "michelle@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg"
+    },
+    {
+        id: 8,
+        name: "Victor de Oliveira",
+        title: "Corretor Sênior",
+        phone: "617-244-3672",
+        mobilePhone: "617-244-3672",
+        email: "victor@ionicrealty.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg"
+    }
 ];
 
 exports.data = brokers;
