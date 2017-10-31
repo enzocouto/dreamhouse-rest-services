a9743882ac2fd242f0351ecafa4768b7bf6610db
enzocouto <enzocouto@gmail.com>
2017-10-31 19:24:19 -0200

    mock de imagens

---
+++ server/mock-imagesProperties.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let images_properties = [
+    {
+        id: 1,
+        images: "https://img.tecnisa.com.br/images/imoveis/128/Foto/piscina_flex_osasco_tecnisa.jpg"
+    },
+    {
+        id: 2,
+        images: "https://img.tecnisa.com.br/images/imoveis/128/Ilustracao/salao_de_festas_adulto_flex_osasco_tecnisa_full.jpg"
+    },
+    {
+        id: 3,
+        images: "http://img.tecnisa.com.br/arquivos/estagioobra/128/Imagem/clube_e_piscinas_flex_osasco_201501_tecnisa.jpg"
+    },
+    {
+        id: 4,
+        images: "https://img.tecnisa.com.br/arquivos/estagioobra/219/Imagem/quadra_flex_osasco_ii_201606_tecnisa.jpg"
+    },
+    {
+        id: 5,
+        images: "https://img.tecnisa.com.br/arquivos/estagioobra/128/Imagem/vista_condominio_flex_osasco_201504_tecnisa.jpg"
+    },
+    {
+        id: 6,
+        images: "https://img.tecnisa.com.br/arquivos/estagioobra/128/Imagem/periferia_flex_osasco_201505_tecnisa.jpg"
+    },
+    {
+        id: 7,
+        images: "https://resizedimgs.vivareal.com/LKwFr7c_eujvzUEdqT9JUFdg3JE=/fit-in/870x653/vr.images.sp/a61859635e478e9b886f2044c892f512.jpg"
+    },
+    {
+        id: 8,
+        images: "http://www.arrobacasa.com.br/img/2011/11/04/flex-osasco-fase-1/implantacao-flex-osasco-fase-1.jpg"
+    },
+    {
+        id: 9,
+        images: "https://img.tecnisa.com.br/images/imoveis/219/Planta/73,54m_-_2_dorms_-_garden_flex_osasco_ii_tecnisa_full.jpg"
+    },
+    {
+        id: 10,
+        images: "https://img.tecnisa.com.br/images/imoveis/219/Video/130347988028054237_thumbVideo.jpg"
+    }
+];
+
+exports.data = images_properties;
